fa7483ca5bec773450e360b9313a86ddd393362f
Andreas.Nawrath <Andreas.Nawrath@NISLP7750-1>
2023-03-06 16:13:50 +0100

    Test

--- SCCplusplusSortLinear/SCCplusplusSortLinear.h
+++ SCCplusplusSortLinear/SCCplusplusSortLinear.h
@@ -1,4 +1,4 @@
 // SCCplusplusSortLinear.h - Standard Codes C++ Sort Linear: contains all Functions for the linear sorting algorythm
 #pragma once
 
-extern "C" __declspec(dllexport) bool SortLinear(int iListArray[], int iSeach);
+extern "C" __declspec(dllexport) bool SortLinear(int *iListArray, int iSeach);
